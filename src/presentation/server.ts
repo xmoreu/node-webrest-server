6117f0b8634b702bd70ef7dc8ab547c6e963f097
Xavi <farmoreu@gmail.com>
2025-05-01 18:11:37 +0200

    Tipo arreglado

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -28,7 +28,7 @@ export class Server{
             })
 
             this.app.listen(this.port,()=>{
-                console.log(`Server running on port ${3000}`)
+                console.log(`Server running on port number ${3000}`)
             })
         }
 }
