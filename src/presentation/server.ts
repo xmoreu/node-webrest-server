8440f54269a7b7c9e2b00377088c524bb46b188f
Xavi <farmoreu@gmail.com>
2025-05-01 22:16:07 +0200

    canvis

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,34 +1,43 @@
-import express from 'express'
+import express, { Router } from 'express'
 import path from 'path'
-interface Options{
-    port:number,
-    public_path?:string
+interface Options {
+    port: number,
+    public_path?: string,
+    routes:Router
 }
-export class Server{
-        private  app=express()
-        private readonly port:number
-        private readonly publicPath:string
-        constructor(options:Options){
-            const {port,public_path}=options
-            this.port=port
-            this.publicPath=public_path ?? 'public'
-        }
-        
-        async start(){
-// Middlewares
+export class Server {
+    private app = express()
+    private readonly port: number
+    private readonly publicPath: string
+    private readonly routes:Router
 
+    constructor(options: Options) {
+        const { port, public_path ,routes} = options
+        this.port = port
+        this.publicPath = public_path ?? 'public'
+        this.routes=routes
+    }
+
+    async start() {
+        // Middlewares
+        this.app.use(express.json())  //Permet llegir json pel raw
+        this.app.use(express.urlencoded({extended:true})) //Permet llegir format x-www-form-urlencode
         //Public folder
-            this.app.use(express.static(this.publicPath))
+        this.app.use(express.static(this.publicPath))
+
+        //Routes
+        this.app.use(this.routes)
+       
 
 
-            this.app.get('/*path',(req,res)=>{
-                const indexPath=path.join(__dirname+`../../../${this.publicPath}/index.html`)
-                res.sendFile(indexPath)
-                return
-            })
+        this.app.get('/*path', (req, res) => {
+            const indexPath = path.join(__dirname + `../../../${this.publicPath}/index.html`)
+            res.sendFile(indexPath)
+            return
+        })
 
-            this.app.listen(this.port,()=>{
-                console.log(`Server running on port number ${3000}`)
-            })
-        }
+        this.app.listen(this.port, () => {
+            console.log(`Server running on port number ${3000}`)
+        })
+    }
 }
